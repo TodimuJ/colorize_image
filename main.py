ad293136ade0897f76c02139efa33618d442456c
TodimuJ <todimujenrola@gmail.com>
2023-06-13 19:44:08 -0400

    Main

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import cv2
 
 # Models: https://github.com/richzhang/colorization/tree/caffe/colorization/models
 # Points: https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
+
+prototxt_path = 'models/colorization_deploy_v2.prototxt'
+model_path = 'models/colorization_release_v2.caffemodel'
+kernel_path = 'models/pts_in_hull.npy'
+image_path = 'lion.jpg'
+
+net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
