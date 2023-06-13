5ce61b2df095e432ab6b1e5a177f57590a314913
TodimuJ <todimujenrola@gmail.com>
2023-06-13 19:36:37 -0400

    Main

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import numpy as np
+import cv2
+
+# Models: https://github.com/richzhang/colorization/tree/caffe/colorization/models
+# Points: https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
